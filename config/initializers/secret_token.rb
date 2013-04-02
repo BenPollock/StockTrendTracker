5a8b839930609b34c37b55f9fe674bd569507cca
Ben Pollock <B.O.Pollock@gmail.com>
2013-04-01 23:29:12 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stocktrends::Application.config.secret_token = '6e1ff0ebd8e77b8b34c52278f4d1b77fd916712101cafdf3e7df86f0866f4189646e372bb154ef80c8409fcf554927a21c76117141f31306881f5ac84cfb0861'
