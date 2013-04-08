596a10bf526229876db5e4547b8dcf2e49a51ae4
Ben Pollock <B.O.Pollock@gmail.com>
2013-04-07 21:53:36 -0400

    Add datepicker, search by date range, total caluclations

--- app/controllers/closes_controller.rb
+++ app/controllers/closes_controller.rb
@@ -4,11 +4,36 @@ class ClosesController < ApplicationController
   	#get value from forms
   	@index = params[:index]
   	@id = params[:id]
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+    @low_range = params[:low_range]
+    @high_range = params[:high_range]
 
+    @f_start = Date.parse(@start_date)
+    @f_end = Date.parse(@end_date)
 
-  	#find the case(s)
+    @cases_nextday = []
+
   	begin
-  		@cases = Close.find(@id)
+      #Find the cases
+      @cases = Close.find(:all, conditions:["date between ? and ?",
+        @f_start, @f_end])
+
+      #Find the next day cases
+      @cases.each do |case_instance|
+        @cases_nextday.push(Close.find(case_instance.id - 1))
+      end
+
+      #Get the average
+      @total = 0
+      @cases_nextday.each do |nextday_instance|
+        instance = (nextday_instance.close-nextday_instance.open)/(nextday_instance.open)
+        @total = @total + instance
+      end
+
+      if(@cases.empty?)
+         @cases = false
+      end
   	rescue
   		@cases = false
   	end
